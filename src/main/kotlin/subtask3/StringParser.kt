bca48d1f7d36bed9862b8fb93d47e3b56b896b06
smailic05 <murad0660@gmail.com>
2021-05-24 17:18:02 +0300

    Update StringParser.kt

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -2,8 +2,47 @@ package subtask3
 
 class StringParser {
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        var str: String
+        val resultList = mutableListOf<String>()
+        for (i in inputString.indices) {
+            when (inputString[i]) {
+                '<' -> {
+                    var count = 0
+                    str = inputString.substring(i + 1)
+                    for (j in str.indices) {
+                        if (str[j] == '<') count++
+                        if (str[j] == '>' && count == 0) {
+                            resultList.add(str.substring(0, j))
+                            break
+                        } else if (str[j] == '>' && count > 0) count--
+                    }
+                }
+
+                '(' -> {
+                    var count = 0
+                    str = inputString.substring(i + 1)
+                    for (j in str.indices) {
+                        if (str[j] == '(') count++
+                        if (str[j] == ')' && count == 0) {
+                            resultList.add(str.substring(0, j))
+                            break
+                        } else if (str[j] == ')' && count > 0) count--
+                    }
+                }
+                '[' -> {
+                    var count = 0
+                    str = inputString.substring(i + 1)
+                    for (j in str.indices) {
+                        if (str[j] == '[') count++
+                        if (str[j] == ']' && count == 0) {
+                            resultList.add(str.substring(0, j))
+                            break
+                        } else if (str[j] == '[' && count > 0) count--
+                    }
+                }
+            }
+        }
+        return resultList.toTypedArray()
     }
 }
